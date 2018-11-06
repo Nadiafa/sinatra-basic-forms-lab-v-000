e7ec8bdcf40b6757842df19b2e0e17290813ff8e
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-11-06 11:15:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,15 +6,20 @@ class App < Sinatra::Base
     erb :index
   end 
   
-  get '/create_puppy' do
+  get '/new' do
     erb :create_puppy
   end
   
-  # post '/create_puppy' do
-  #   erb :display_puppy
-  # end
-  
-  get '/new' do 
+  post '/display' do
     erb :display_puppy
   end
+
 end
+
+
+  post '/puppy' do
+    @name = params['name']
+    @age = params['age']
+    @breed = params['breed']
+    erb :display_puppy
+  end
